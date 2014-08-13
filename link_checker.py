d42f80c43afbad52dc9213e10f7d4f3b7506bf51
Mike Conlon <mconlon@ufl.edu>
2014-08-13 17:43:21 -0400

    Exception handling and code formatting

--- link_checker.py
+++ link_checker.py
@@ -7,18 +7,23 @@
     --  basic framing, find all broken links
     Verrion 0.1 MC 2013-11-28
     --  Works as expected
+    Version 0.2 MC 2014-08-13
+    --  Improve code formatting, datetime stamping of all output, eror handling,
+        passes pylint
 
 """
 
-__author__      = "Michael Conlon"
-__copyright__   = "Copyright 2013, University of Florida"
-__license__     = "BSD 3-Clause license"
-__version__     = "0.1"
-    
-import vivotools as vt
+__author__ = "Michael Conlon"
+__copyright__ = "Copyright 2013, University of Florida"
+__license__ = "BSD 3-Clause license"
+__version__ = "0.2"
+
 from datetime import datetime
+from vivotools import rdf_header
+from vivotools import rdf_footer
+from vivotools import remove_uri
+from vivotools import vivo_sparql_query
 import urllib
-import sys
 
 query = """
 SELECT ?uri ?w ?linkuri
@@ -29,42 +34,55 @@ WHERE {
 """
 
 status = {}
-srdf = vt.rdf_header()
-print datetime.now()
-data = vt.vivo_sparql_query(query)["results"]["bindings"]
-print "Links found = ",len(data)
+srdf = rdf_header()
+print datetime.now(), "Start"
+data = vivo_sparql_query(query)["results"]["bindings"]
+print datetime.now(), "Links found = ", len(data)
 i = 0
 for item in data:
     i = i + 1
     if i % 500 == 0:
-        print i
+        print datetime.now(), i
     uri = item["uri"]["value"]
     weburi = item["w"]["value"]
     linkuri = item["linkuri"]["value"]
     try:
         code = urllib.urlopen(linkuri).getcode()
         if code == None:
-            status["None"] = status.get("None",0) + 1
+            status["None"] = status.get("None", 0) + 1
         else:
-            status[str(code)] = status.get(str(code),0) + 1
-    except:
-        print "No code",None,i,linkuri
-        status["Exception"] = status.get("Exception",0) + 1
+            status[str(code)] = status.get(str(code), 0) + 1
+    except IOError:
+        print datetime.now(), "IOError", None, i, linkuri
+        status["IOError"] = status.get("IOError", 0) + 1
         continue
-    if code == 200:
+    else:
+        print datetime.now(), "Other Exception", None, i, linkuri
+        status["Other Exception"] = status.get("Other Exception", 0) + 1
+        continue
+    if code <= 200 or code == 403 or code == 401:
         continue
     elif code == 404 or code == 410:
-        print "Remove",code,i,linkuri,"from",uri
+        print datetime.now(), "Remove", code, i, linkuri, "from", uri
 
         # Remove all facts about the web page and all references to
         # the web page
-        
-        sub = vt.remove_uri(weburi)
+
+        sub = remove_uri(weburi)
         srdf = srdf + sub
     else:
-        print "Unknown code",code,i,linkuri
-srdf = srdf + vt.rdf_footer()
-print datetime.now()
+        print datetime.now(), "Unknown code", code, i, linkuri
+
+#    Print code frequencies
+
 for key in sorted(status.keys()):
-    print key,status[key]
+    print key, status[key]
+
+#   Write out subtraction RDF
+
+srdf = srdf + rdf_footer()
 print srdf
+
+#   All done
+
+print datetime.now(), "Finished"
